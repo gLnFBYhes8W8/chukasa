f88daa4092e7ed05278d35deb0b025dc65704b56
hirooka <dev@hirooka.pro>
2017-08-08 23:38:49 +0900

    [change] APIはBasic認証にしてみた

--- chukasa-aaa/src/main/java/pro/hirooka/chukasa/chukasa_aaa/domain/configuration/WebSecurityConfiguration.java
+++ chukasa-aaa/src/main/java/pro/hirooka/chukasa/chukasa_aaa/domain/configuration/WebSecurityConfiguration.java
@@ -1,62 +1,62 @@
-package pro.hirooka.chukasa.chukasa_aaa.domain.configuration;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import pro.hirooka.chukasa.chukasa_aaa.domain.service.IChukasaUserDetailsService;
-
-@EnableWebSecurity
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    AaaConfiguration aaaConfiguration;
-    @Autowired
-    IChukasaUserDetailsService chukasaUserDetailsService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        if(aaaConfiguration.getProfiles().contains("postgresql")
-                || aaaConfiguration.getProfiles().contains("mysql")
-                || aaaConfiguration.getProfiles().contains("hsqldb")){
-            authenticationManagerBuilder.userDetailsService(this.chukasaUserDetailsService);//.passwordEncoder(passwordEncoder()); // TODO:
-        }else{
-            authenticationManagerBuilder.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-            authenticationManagerBuilder.inMemoryAuthentication().withUser("user").password("user").roles("USER");
-            authenticationManagerBuilder.inMemoryAuthentication().withUser("guest").password("guest").roles("GUEST");
-        }
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/menu")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login")
-                .deleteCookies("JSESSIONID")
-                .invalidateHttpSession(true)
-                .permitAll();
-    }
-
-    @Override
-    public void configure(WebSecurity web){
-        web.ignoring().antMatchers("/images/**", "/webjars/**");
-    }
-
-}
+//package pro.hirooka.chukasa.chukasa_aaa.domain.configuration;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//import pro.hirooka.chukasa.chukasa_aaa.domain.service.IChukasaUserDetailsService;
+//
+//@EnableWebSecurity
+//@Configuration
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    AaaConfiguration aaaConfiguration;
+//    @Autowired
+//    IChukasaUserDetailsService chukasaUserDetailsService;
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        if(aaaConfiguration.getProfiles().contains("postgresql")
+//                || aaaConfiguration.getProfiles().contains("mysql")
+//                || aaaConfiguration.getProfiles().contains("hsqldb")){
+//            authenticationManagerBuilder.userDetailsService(this.chukasaUserDetailsService);//.passwordEncoder(passwordEncoder()); // TODO:
+//        }else{
+//            authenticationManagerBuilder.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+//            authenticationManagerBuilder.inMemoryAuthentication().withUser("user").password("user").roles("USER");
+//            authenticationManagerBuilder.inMemoryAuthentication().withUser("guest").password("guest").roles("GUEST");
+//        }
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/menu")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/login")
+//                .deleteCookies("JSESSIONID")
+//                .invalidateHttpSession(true)
+//                .permitAll();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web){
+//        web.ignoring().antMatchers("/images/**", "/webjars/**");
+//    }
+//
+//}
