4670ef54e0b4757ce78d2c511bd5ab4c7f7aafb4
hirooka <dev@hirooka.pro>
2018-05-04 12:18:08 +0900

    [update] デフォルト値変更

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,2 +1,2 @@
 /usr/local/nginx/sbin/nginx
-java -Djava.security.egd=file:/dev/./urandom -jar -Dspring.profiles.active=localhost-no-database /chukasa.jar
+java -Djava.security.egd=file:/dev/./urandom -jar -Dspring.profiles.active=localhost-docker-mongodb-postgresql /chukasa.jar
