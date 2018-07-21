fa3104328d62a56c3495089ea2de019bfeb16c48
hirooka <dev@hirooka.pro>
2018-07-21 21:24:23 +0900

    [ref] RestController

--- src/main/java/pro/hirooka/chukasa/api/v1/HelloRestController.java
+++ src/main/java/pro/hirooka/chukasa/api/v1/HelloRestController.java
@@ -1,16 +1,25 @@
 package pro.hirooka.chukasa.api.v1;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pro.hirooka.chukasa.domain.model.api.ChukasaResponse;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/hello")
 public class HelloRestController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("")
     public ChukasaResponse hello(){
-        return new ChukasaResponse("hello");
+        final Instant instant = Instant.ofEpochMilli(new Date().getTime());
+        final ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault());
+        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_ZONED_DATE_TIME;
+        return new ChukasaResponse(zonedDateTime.format(dateTimeFormatter) + "|" + zonedDateTime.toEpochSecond());
     }
 }
--- src/main/java/pro/hirooka/chukasa/api/v1/HlsPlayerRestController.java
+++ src/main/java/pro/hirooka/chukasa/api/v1/HlsPlayerRestController.java
@@ -4,139 +4,84 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.hirooka.chukasa.api.v1.exception.ChukasaBadRequestException;
 import pro.hirooka.chukasa.api.v1.exception.ChukasaInternalServerErrorException;
 import pro.hirooka.chukasa.api.v1.helper.ChukasaUtility;
+import pro.hirooka.chukasa.domain.activity.IFileActivity;
+import pro.hirooka.chukasa.domain.activity.IHlsActivity;
+import pro.hirooka.chukasa.domain.activity.IProgramActivity;
+import pro.hirooka.chukasa.domain.activity.ISystemActivity;
 import pro.hirooka.chukasa.domain.config.ChukasaConstants;
-import pro.hirooka.chukasa.domain.config.common.SystemConfiguration;
-import pro.hirooka.chukasa.domain.config.common.type.FfmpegVcodecType;
-import pro.hirooka.chukasa.domain.config.hls.HlsConfiguration;
 import pro.hirooka.chukasa.domain.model.api.ChukasaResponse;
 import pro.hirooka.chukasa.domain.model.api.HlsPlaylist;
 import pro.hirooka.chukasa.domain.model.hls.ChukasaModel;
 import pro.hirooka.chukasa.domain.model.hls.ChukasaSettings;
-import pro.hirooka.chukasa.domain.service.common.ISystemService;
-import pro.hirooka.chukasa.domain.service.hls.IChukasaModelManagementComponent;
-import pro.hirooka.chukasa.domain.service.hls.ICoordinatorService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.UUID;
 
-// TODO: refactor
+import static java.util.Objects.requireNonNull;
 
 @RestController
-@RequestMapping("api/v1/hls")
+@RequestMapping("api/v1/chukasa")
 public class  HlsPlayerRestController {
 
     private static final Logger log = LoggerFactory.getLogger(HlsPlayerRestController.class);
 
-    @Autowired
-    SystemConfiguration systemConfiguration;
-    @Autowired
-    HlsConfiguration hlsConfiguration;
-    @Autowired
-    IChukasaModelManagementComponent chukasaModelManagementComponent;
-    @Autowired
-    HttpServletRequest httpServletRequest;
-    @Autowired
-    ISystemService systemService;
-    @Autowired
-    ICoordinatorService taskCoordinatorService;
+    private final HttpServletRequest httpServletRequest;
+    private final IHlsActivity hlsActivity;
+    private final IFileActivity fileActivity;
+    private final ISystemActivity systemActivity;
+    private final IProgramActivity programActivity;
 
-    @RequestMapping(value = "/start", method = RequestMethod.POST)
+    @Autowired
+    public HlsPlayerRestController(
+            HttpServletRequest httpServletRequest,
+            IHlsActivity hlsActivity,
+            IFileActivity fileActivity,
+            ISystemActivity systemActivity,
+            IProgramActivity programActivity) {
+        this.httpServletRequest = requireNonNull(httpServletRequest);
+        this.hlsActivity = requireNonNull(hlsActivity);
+        this.fileActivity = requireNonNull(fileActivity);
+        this.systemActivity = requireNonNull(systemActivity);
+        this.programActivity = requireNonNull(programActivity);
+    }
+
+    @PostMapping(value = "/start")
     HlsPlaylist play(@RequestBody @Validated ChukasaSettings chukasaSettings) throws ChukasaBadRequestException, ChukasaInternalServerErrorException {
 
-        String userAgent = httpServletRequest.getHeader("user-agent");
+        final String userAgent = httpServletRequest.getHeader("user-agent");
         log.info("user-agent: {}", userAgent);
         if(!userAgent.contains(ChukasaConstants.USER_AGENT)){
             throw new ChukasaBadRequestException("User-Agent is invalid");
         }
 
-        FfmpegVcodecType ffmpegVcodecType = systemService.getFfmpegVcodecType(userAgent);
-        if(ffmpegVcodecType == FfmpegVcodecType.UNKNOWN){
-            throw new ChukasaInternalServerErrorException("FFmpeg configuration is not suitable for this application.");
-        }
-
-        ChukasaUtility.initializeRunner(chukasaModelManagementComponent, systemConfiguration);
-        if(chukasaModelManagementComponent.get().size() > 0){
-            throw new ChukasaInternalServerErrorException("Cannot start streaming bacause previous one is not finished.");
-        }
-
-        log.info("ChukasaSettings -> {}", chukasaSettings.toString());
-
-        ChukasaModel chukasaModel = new ChukasaModel();
-        chukasaModel.setSystemConfiguration(systemConfiguration);
-        chukasaModel.setHlsConfiguration(hlsConfiguration);
-        chukasaModel.setChukasaSettings(chukasaSettings);
-
-        chukasaModel.setUuid(UUID.randomUUID());
-        chukasaModel.setAdaptiveBitrateStreaming(0);
-        chukasaModel.setFfmpegVcodecType(ffmpegVcodecType);
-        // TODO: -> system service
-        if(ffmpegVcodecType == FfmpegVcodecType.HEVC_NVENC || ffmpegVcodecType == FfmpegVcodecType.HEVC_QSV){
-            chukasaModel.setStreamFileExtension(".m4s");
-        }
-
-        chukasaModel = ChukasaUtility.operateEncodingSettings(chukasaModel);
-        if(chukasaModel == null){
-            throw new ChukasaBadRequestException("TranscodingSettings is invalid");
-        }
-
         String servletRealPath = httpServletRequest.getSession().getServletContext().getRealPath("");
-        String streamRootPath = systemService.getStreamRootPath(servletRealPath);
-        chukasaModel.setStreamRootPath(streamRootPath);
-        chukasaModel = ChukasaUtility.createChukasaDerectory(chukasaModel);
-        chukasaModel = ChukasaUtility.calculateTimerTaskParameter(chukasaModel);
+        log.info("servletRealPath = {}", servletRealPath);
 
-        String playlistURI = ChukasaUtility.buildM3u8URI(chukasaModel);
+        final ChukasaModel chukasaModel = hlsActivity.startPlayback(chukasaSettings, userAgent, servletRealPath);
+        final String playlistURI = ChukasaUtility.buildM3u8URI(chukasaModel);
         if(playlistURI.equals("/")){
-            throw new ChukasaInternalServerErrorException("Cannot create playlist.");
+            //
         }
 
-        chukasaModelManagementComponent.create(0, chukasaModel);
-
-        //chukasaTaskService.execute(0);
-        taskCoordinatorService.execute();
-
-        HlsPlaylist hlsPlaylist = new HlsPlaylist();
+        final HlsPlaylist hlsPlaylist = new HlsPlaylist();
         hlsPlaylist.setUri(playlistURI);
         return hlsPlaylist;
     }
 
-    @RequestMapping(value = "/stop", method = RequestMethod.GET)
-    ChukasaResponse stop() throws ChukasaInternalServerErrorException {
-        taskCoordinatorService.cancel();
-        taskCoordinatorService.stop();
-        //chukasaStopper.stop();
+    @GetMapping(value = "/stop")
+    ChukasaResponse stop() {
+        hlsActivity.stopPlayback();
         return remove();
     }
 
-    @RequestMapping(value = "/remove", method = RequestMethod.GET)
-    ChukasaResponse remove() throws ChukasaInternalServerErrorException {
-        removeStreamingFiles();
+    @GetMapping(value = "/remove")
+    ChukasaResponse remove() {
+        hlsActivity.removeStream();
         ChukasaResponse chukasaResponseModel = new ChukasaResponse();
         chukasaResponseModel.setMessage("Streaming stopped successfully.");
         return chukasaResponseModel;
     }
-
-    private void removeStreamingFiles(){
-        taskCoordinatorService.remove();
-        //taskCoordinatorService.cancel();
-    }
-
-//    private void removeStreamingFiles() throws ChukasaInternalServerErrorException {
-//        String streamRootPath = commonUtilityService.getStreamRootPath(httpServletRequest.getSession().getServletContext().getRealPath(""));
-//        if(Files.exists(new File(streamRootPath).toPath())) {
-//            chukasaRemover.setStreamRootPath(streamRootPath);
-//            chukasaRemover.remove();
-//        }else {
-//            log.warn("cannot remove files bacause streamRootPath: {} does not exist.", streamRootPath);
-//            throw new ChukasaInternalServerErrorException("Cannot remove files bacause streamRootPath does not exist.");
-//        }
-//    }
 }
-
